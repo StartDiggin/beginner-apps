38b71e1d0cd1878ed7b9d75abb36a4699507c136
Johnny Gonzales <dimensions122@gmail.com>
2020-12-07 12:04:25 -0500

    random color generator finished

--- src/components/APPS/RandomColor.js
+++ src/components/APPS/RandomColor.js
@@ -9,7 +9,8 @@ class RandomColor extends React.Component {
     state={
         color: "red",
         hexColor:"#000",
-        rgbaColor: "rgba(0,1,2,0.3)"
+        rgbaColor: "rgba(0,1,2,0.3)",
+        hslaColor:"hsla"
     }
 
 
@@ -28,6 +29,7 @@ class RandomColor extends React.Component {
         })
     }
 
+    // returns a random rgba color 
     rgbaColor = () => {
         let rgbaC = "rgba(0,1,2,3)"
         let num1 = Math.floor(Math.random() * 256)
@@ -38,9 +40,21 @@ class RandomColor extends React.Component {
         this.setState({
             rgbaColor: rgbaC
         })
-        
     }
+    
+    // returns a random hsla color 
+    hslaColor = () => {
+        let hslaC = "";
+        let num1 = Math.floor(Math.random() * 361)
+        let num2 = Math.floor(Math.random().toFixed(2) * 100) + "%"
+        let num3 = Math.floor(Math.random().toFixed(2) * 100) + "%"
+    
+        hslaC = `hsla(${num1},${num2},${num3})`
+        this.setState({
+            hslaColor: hslaC
+        })
 
+    }
 
 
 
@@ -51,14 +65,23 @@ class RandomColor extends React.Component {
         const styles1={
             color: this.state.rgbaColor
         }
+        const styles2={
+            color: this.state.hslaColor
+        }
         return(
             <div>
+                {/* hex color  */}
                 <h1 style={styles}>Random Hex Color</h1>
-                <h4>Hex number:{this.state.hexColor}</h4>
+                <h4>Hex color: {this.state.hexColor}</h4>
                 <button onClick={this.hexColor}>Hex color</button>
+                {/* rgba color  */}
                 <h1 style={styles1}>Random RGBA Color</h1>
-                <h4>RGBA number:{this.state.rgbaColor}</h4>
+                <h4>RGBA color: {this.state.rgbaColor}</h4>
                 <button onClick={this.rgbaColor}>RGBA color</button>
+                {/* hsla color  */}
+                <h1 style={styles2}>Random HSLA Color</h1>
+                <h4>HSLA color: {this.state.hslaColor}</h4>
+                <button onClick={this.hslaColor}>HSLA color</button>
             </div>
         )
 
