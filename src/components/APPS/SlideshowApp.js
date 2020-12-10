b1d84afbf91e19ca9d2d2cd4e572cdc29c58ec91
Johnny Gonzales <dimensions122@gmail.com>
2020-12-10 10:49:59 -0500

    working on img carousel

--- src/components/APPS/SlideshowApp.js
+++ src/components/APPS/SlideshowApp.js
@@ -7,37 +7,68 @@ import navNext from './Data/nav_next.svg'
 
 
 class SlideShowApp extends React.Component {
-
-
-
-    handleNext = () => {
-       let imgDivs = [...document.querySelectorAll(".ssImg")]
-       
-        // check id if id is 1 add class to id 2 or 
-       if(imgDivs[0].classList.contains("active")){
-            imgDivs[0].classList.remove("active")
-            imgDivs[1].classList.add("active")
-       };
+    state={
+        slideIndex: 2,
+        imgs:[]
     }
+
+
+    // componentDidMount() {
+    //     this.showSlides(1)
+    // }
+
+    showSlides = () => {
+        let slides = document.querySelectorAll('.myslides')
+        for(let i=0;i<slides.length;i++){
+            slides[i].classList.remove('active')
+        }
+        let num = this.state.slideIndex
+        slides[num-1].classList.add('active')
+
+    }   
+
+    handleClick = () => {
+        console.log('next photo')
+    }
+
+    changeSlide = (n) => {
+        let slides = document.querySelectorAll('.myslides')
+        let len = slides.length
+        let index = this.state.slideIndex
+        index += n
+
+        index < 1 ? this.setState({slideIndex:len}) : index > len ? this.setState({slideIndex: 1}) : this.setState({slideIndex:index})
+        
+        this.showSlides()
+        console.log(this.state.slideIndex)
+    }
+
+
+
+
+
+
 
     render(){
         return(
             <div className="section">
                 <h1>Slide Show App</h1>
-                <div className="ssContainer">
-                    <div className="imgContainer">
-                        <div id="1" className="ssImg active">
-                            <img src={img1} className="imgs" alt="img"/>
-                        </div>
-                        <div id="2" className="ssImg ">
-                            <img src={img2} className="imgs" alt="img"/>
-                        </div>
-                        <div id="3" className="ssImg ">
-                            <img src={img3} className="imgs" alt="img"/>
-                        </div>
-                        <img src={navPrev} className="navPrev" alt="prev"/>
-                        <img src={navNext} onClick={this.handleNext} className="navNext" alt="next"/>
+                <div className="imgContainer">
+                    {/* image 1 */}
+                    <div className="myslides active">
+                        <img src={img1} alt="img"/>
                     </div>
+                    {/* image 2  */}
+                    <div className="myslides">
+                        <img src={img2} alt="img"/>
+                    </div>
+                    {/* image 3 */}
+                    <div className="myslides">
+                        <img src={img3} alt="img"/>
+                    </div>
+
+                    <img src={navPrev} onClick={() => this.changeSlide(-1)} className="prev" alt="prev"/>
+                    <img src={navNext} onClick={() => this.changeSlide(1)} className="next" alt="next"/>
                 </div>
             </div>
         )
