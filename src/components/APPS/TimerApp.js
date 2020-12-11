5c651e58b3f4ff17db3fcc987ac093a9029dcebd
Johnny Gonzales <dimensions122@gmail.com>
2020-12-11 12:26:26 -0500

    got the timer working

--- src/components/APPS/TimerApp.js
+++ src/components/APPS/TimerApp.js
@@ -1,33 +1,57 @@
-import React, { useState } from 'react'
+import React, {useState, useEffect } from 'react'
 
 
 
-let nowTime = setInterval(function(){
-    let now = new Date().getTime()
-    // console.
-}, 1000)
 
 const TimerApp = () => {
-    const [ lunch, setLunch ] = useState(5)
+    const [counter, setCounter] = useState(0)
+    const [mins, setMins] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [breaks, setBreaks] = useState(15)
     
-    const [ session, setSession ] = useState(25)
     
-    console.log({lunch},{session})
+
+    useEffect(() => {
+        const timer = 
+            counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+            return () => clearInterval(timer);
+    },  [counter]);
+
+    useEffect(() => {
+            counter === 0 && mins > 0 && setTimeout(() => setMins(mins -1), setCounter(59), 1000);
+    },[counter, mins])
+
+    useEffect(() => {
+        
+    },[minutes])
+
+
+
+   
+
+  
+    
+   
     return(
         <div className="section">
             <h1>Timer App</h1>
             <div className="timer">
-                <div><span>Break Time:</span></div>
+                <div>
+                    <span>Break Time:</span>
+                    <h3>{breaks} minutes</h3>    
+                </div>
                 <div>
                     <span>Timer:</span>
-                    <h3>{session}</h3>
+                    <h3>{mins}:{counter}</h3>
+                    <button onClick={() => {setMins(minutes); setCounter(59)}}>Start Timer</button>
                 </div>
                 <div> 
                     <span>Session Time:</span>
-                    <button onClick={() => setLunch(lunch + 5)}>add time</button>
-                    <button onClick={() => setLunch(lunch - 5)}>minus time</button>
-                    <button onClick={() => console.log(nowTime)}>asdafs</button>
-                   
+                    <h3>{minutes}</h3>
+                    <button onClick={() => setMinutes(minutes + 5)}>add 5 minutes</button>
+                    <button onClick={() => setMinutes(minutes - 5)}>minus 5 minutes</button>
+                </div>
+                <div className="btns">
                 </div>
                 
                 
@@ -35,6 +59,10 @@ const TimerApp = () => {
             </div>
         </div>
     )
+
 }
+
+
+
 
 export default TimerApp
