346d9b1ba0ca01d75fd8236d41076bf4f13a7ad0
Johnny Gonzales <dimensions122@gmail.com>
2020-12-13 14:59:12 -0500

    having difficulty with css and clearing the caches

--- src/components/APPS/TimerApp.js
+++ src/components/APPS/TimerApp.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect } from 'react'
 
+
 import bells from './Data/bells.wav'
 // import clap from './Data/clap.wav'
+
+
 
 
 
@@ -14,28 +17,29 @@ const TimerApp = () => {
     const [breaks, setBreaks] = useState(0)
     const [ audio ] = useState(new Audio(bells))
     const [ playing, setPlaying ] = useState(false)
-    
-    
 
+    
+    
+    // responsible for countdown timer 
     useEffect(() => {
         const timer = 
             counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
             return () => clearInterval(timer);
     },  [counter]);
 
+    // responsible for interating minutes 
     useEffect(() => {
             counter === 0 && mins > 0 && setTimeout(() => setMins(mins - 1), setCounter(59), 1000);
     },[counter, mins])
 
+    // sets playing flag to true if start flag is true 
    useEffect(() => {
        if(start === true){
            setPlaying(true)
-           console.log('started')
         }
     },[start])
     
-    
-    
+    // responsible for playing sound when timer is finished 
     useEffect(() => {
         if(playing === true && mins === 0 && counter === 0){
             audio.play()
@@ -43,15 +47,24 @@ const TimerApp = () => {
         }
     })
 
-  
+    // function button(){
+    //     let btn = document.querySelector(".useBreakBtn")
+    //     btn.classList.add("active")
+    // }
     
+
+
    
     return(
         <div className="section">
             <h1>Timer App</h1>
             <div className="timer">
                 <div>
-                    <span>Break Time:</span>
+                    <span>Break Time:</span><br />
+                    <button clasName=".useBreakBtn" onClick={() => {
+                        let btn = document.querySelector("useBreakBtn")
+                        btn.classList.toggle("activeTimer")
+                    }} >On Off</button>
                     <h3>{breaks} minutes</h3>   
                     <button onClick={() => setBreaks(breaks + 5)}>add 5 minutes</button>
                     {breaks > 4 ? <button onClick={() => setBreaks(breaks - 5)}>minus 5 minutes</button> : null}
@@ -67,9 +80,10 @@ const TimerApp = () => {
                 <div> 
                     <span>Session Time:</span>
                     <h3>{minutes}</h3>
-                    <button onClick={() => setMinutes(minutes + 5)}>add 5 minutes</button>
-                    <button onClick={() => setMinutes(minutes - 5)}>minus 5 minutes</button>
-                    <button onClick={() => setMinutes(minutes - 5)}>minus 5 minutes</button>
+                    <button onClick={() => setMinutes(minutes + 5) }>add 5 minutes</button>
+                    <button onClick={() => setMinutes(minutes - 5) }>minus 5 minutes</button>
+                </div>
+                <div>
                 </div>
                 
             </div>
