ff218331769520b716f15401408993166b7800e7
Johnny Gonzales <dimensions122@gmail.com>
2020-12-14 09:24:26 -0500

    working on functionality of timer using hooks

--- src/components/APPS/TimerApp.js
+++ src/components/APPS/TimerApp.js
@@ -10,11 +10,16 @@ import bells from './Data/bells.wav'
 
 
 const TimerApp = () => {
+    // main timer variables 
     const [counter, setCounter] = useState(0)
     const [mins, setMins] = useState(0)
     const [minutes, setMinutes] = useState(0)
     const [start, setStart] = useState(false)
+    // break variables 
     const [breaks, setBreaks] = useState(0)
+    const [breakMins, setBreakMins] =useState(0)
+    // const [breakCounter, setBreakCounter] = useState(0)
+    // audio variables 
     const [ audio ] = useState(new Audio(bells))
     const [ playing, setPlaying ] = useState(false)
 
@@ -29,8 +34,8 @@ const TimerApp = () => {
 
     // responsible for interating minutes 
     useEffect(() => {
-            counter === 0 && mins > 0 && setTimeout(() => setMins(mins - 1), setCounter(59), 1000);
-    },[counter, mins])
+            counter === 0 && mins > 0 && setTimeout(() => setMins(mins - 1), setBreakMins(breakMins - 1), setCounter(59), 1000);
+    },[counter, mins, breakMins])
 
     // sets playing flag to true if start flag is true 
    useEffect(() => {
@@ -41,17 +46,13 @@ const TimerApp = () => {
     
     // responsible for playing sound when timer is finished 
     useEffect(() => {
-        if(playing === true && mins === 0 && counter === 0){
+        if(playing === true && breakMins === 0 && counter === 0 ){
             audio.play()
-            console.log('playing')
         }
     })
 
-    // function button(){
-    //     let btn = document.querySelector(".useBreakBtn")
-    //     btn.classList.add("active")
-    // }
     
+ 
 
 
    
@@ -61,27 +62,32 @@ const TimerApp = () => {
             <div className="timer">
                 <div>
                     <span>Break Time:</span><br />
-                    <button clasName=".useBreakBtn" onClick={() => {
-                        let btn = document.querySelector("useBreakBtn")
-                        btn.classList.toggle("activeTimer")
-                    }} >On Off</button>
                     <h3>{breaks} minutes</h3>   
-                    <button onClick={() => setBreaks(breaks + 5)}>add 5 minutes</button>
+                    <button onClick={() => setBreaks(breaks + 1)}>add 5 minutes</button>
+
+                    {/* minus 5 minutes button doesn't show unless there is 5 minutes are more added first  */}
                     {breaks > 4 ? <button onClick={() => setBreaks(breaks - 5)}>minus 5 minutes</button> : null}
                      
                 </div>
                 <div>
                     <span>Timer:</span>
                     {counter < 10 ? <h3>{mins}:0{counter}</h3>:<h3>{mins}:{counter}</h3>}
-                    {minutes < 5 ? null : <button onClick={() => {setMins(minutes-1); setCounter(59); setStart(true)}}>Start Timer</button>}
-                   
+                    {minutes < 5 ? null : <button onClick={() => {setMins(minutes-1); 
+                        setBreakMins(breaks-1);  
+                        setCounter(59); 
+                        setStart(true)}}
+                    >Start Timer</button>}<br />
+
+                    <span>Break Time Left:</span><br />
+                    {counter < 10 && breakMins > -1 ? <h3>{breakMins}:0{counter}</h3>: breakMins > -1 ? <h3>{breakMins}:{counter}</h3>:null}
+                        
                     
                 </div>
                 <div> 
                     <span>Session Time:</span>
-                    <h3>{minutes}</h3>
+                    <h3>{minutes} minutes</h3>
                     <button onClick={() => setMinutes(minutes + 5) }>add 5 minutes</button>
-                    <button onClick={() => setMinutes(minutes - 5) }>minus 5 minutes</button>
+                    {minutes > 4 ? <button onClick={() => setMinutes(minutes - 5)}>minus 5 minutes</button> : null}
                 </div>
                 <div>
                 </div>
