a6cf63c4f316b10388346477f20b23235d55b845
Johnny Gonzales <dimensions122@gmail.com>
2020-12-16 15:58:11 -0500

    Home app

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,18 @@ const Home = () => {
                 React was a good one to learn. Here are a few of my starter projects. Take a
                 look and I hope this helps anyone who is trying to learn React and is out of 
                 ideas of what to build. Thank you for stopping in, have a good one!!!
+                My name is Johnny, I am working on learning another language and I figured
+                React was a good one to learn. Here are a few of my starter projects. Take a
+                look and I hope this helps anyone who is trying to learn React and is out of 
+                ideas of what to build. Thank you for stopping in, have a good one!!!
+                My name is Johnny, I am working on learning another language and I figured
+                React was a good one to learn. Here are a few of my starter projects. Take a
+                look and I hope this helps anyone who is trying to learn React and is out of 
+                ideas of what to build. Thank you for stopping in, have a good one!!!
+                My name is Johnny, I am working on learning another language and I figured
+                React was a good one to learn. Here are a few of my starter projects. Take a
+                look and I hope this helps anyone who is trying to learn React and is out of 
+                ideas of what to build. Thank you for stopping in, have a good one!!!
             </p>
            
         </div>
